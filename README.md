c03fa20d80501017eac9d8a3b59a29ce5729c29f
Rasmus <rasmus.jansson@stamford.se>
2022-08-25 11:57:12 +0200

    - Changed readme - Added presentation - Added instructions

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# Uppstart
-Hej och välkommen till detta Hackathon! I denna readme har vi samlat den information som ni ska behöva för att tackla det praktiskt runt uppgiften.
-Agendan för dessa dagar finns i ett dokument som heter Agenda.pptx.
-Ni som team jobbar i detta repo under denna tid. Vi i PL gruppen kommer att titta i repot under dagarnas gång och även kolla i det när vi ska utvärdera vinnarna det är därför viktigt att allt arbete sker här så att allt är på samma plats.
+# Instruktioner
+- Installera Rpanion 0.8
+- Modifiera filen /home/pi/Rpanion-server/python/rtsp-server.py - ett exempel finns under instructions-mappen
+  På raderna som börjar med pipeline_str har vi lagt till "config-interval=1" på rtph264pay så att
+	! rtph264pay name=pay0 pt=96
+	blir
+	! rtph264pay name=pay0 pt=96 config-interval=1	
+- Ställ in Rpanion enligt bilderna i instructions-mappen
 
-Ett tips till teamet är att gå varvet runt och låta alla presentera sig, ni kan utgå från detta ifall ni vill:
-* Vem är du?
-* Vad heter du?
-* Vart jobbar du och vad jobbar du med?
-* Vad har du gjort innan din nuvarande anställning?
-* Vilka tekniker jobbar du mest med idag?
-* Är det något under dessa dagar som du redan nu vet att du skulle vilja undersöka mera?
 
-# För att klona repo
-Github har tagit bort stödet för användarnamn och lösenord över HTTPS så ni behöver skapa ett PersonalAccessToken(PAT).
-Gå in under Settings i rullisten uppe i högra hörnet på eran profil. Längst ner i Settings listan finns Developer Settings, klicka på den. Klicka sedan på Personal acces tokens. Klicka sedan på Generate new token. Ge ditt token ett namn och bocka för repo kryssrutan.
-Detta skapar ett token med korrekt OAuth scope och du kan nu klona repot genom att tex köra: git clone https://< token >@github.com/Addnode-Hackaton-2022/Team-3.git
-
-### Första uppgifterna till teamet, deadline dag 1 kl 11:00. Svara direkt i denna readme i punkterna under.
-* Gå igenom pp Agenda.pptx
-* Gå varvet runt och presentera era enligt frågorna ovan.
 * Välj en teamleader för erat team.
   - Teamleader - Victor Blomberg
 * Välj ett namn för erat team. Detta skall även skrivas på dörren.
@@ -26,66 +15,46 @@ Detta skapar ett token med korrekt OAuth scope och du kan nu klona repot genom a
 * Vilken utmaning jobbar ni med?
   - Namn - Korrekt strömning från drönare
  
- 
- ### Andra uppgiften, deadline dag 2 kl 12:30
- * Ett inskick där ni ska fylla på denna readme med information under avsnittet Inskick, deadline dag två kl 13:00
- * En muntlig presentation där ni får presentera eran uppgift.
- * Lösningen som ni jobbat på under dessa dagar.
-
-Det är på dessa moment som eran uppgift blir bedömd.
-
-### Vad händer efter hackatonet?
-En vecka efter hackatonet kommer alla repon att göras publika.
-
-# Viktiga tider
-### Dag 1 kl 11:00 - Deadline uppstart
-### Dag 1 kl 12:00 - Lunch
-### Dag 1 kl 18:30 - Middag
-### Dag 2 kl 08:00 - Uppsamling H4, dagen startar
-### Dag 2 kl 11:00 - Lunch
-### Dag 2 kl 12:30 - Deadline för uppgiften. 
-Här skall sista commiten till repot vara gjord och den muntliga presentationen vara klar för redovisning.
-### Dag 2 kl 15:00 - Hackaton avslutas med mingel för de som har möjlighet
-
-# Inskick, deadline dag två kl 12:30
 
 ### Övergripande beskrivning och val av utmaning
-<beskrivning här>
+Korrekt strömning från drönare
 
 ### Team
 
 #### Namn på medlemmar 
-* <namn.>
-* <namn.>
-* <osv...>
+* Victor Blomberg
+* Scott Levkowetz
+* Fredrik Lemón Larsson
+* Marcus Johansson
+* Victor Böhmer
+* Rasmus Jansson
 
 #### Hur har ni jobbat inom teamet? Har alla gjort samma eller har ni haft olika roller?
-<beskrivning.>
+Vi har delat upp arbetet utefter vilka uppgifter och utmaningar som har uppenbarat sig.
 
 ### Teknik. Beskrivningen på eran teknikstack, språk och APIer ni har använt.
-<beskrivning.>
+Python, C#
 
 ### Lösning, dessa frågor ska minst besvaras
  * Hur har ni löst utmaningen?
+Vi har med hjälp av ett python-script plockat ut den telemetridata som är relevant för videostabiliseringen.
+Skriptet kombinerar sedan datan med videoströmmen och skickar vidare TCP-paket.
+För att bevisa att det fungerar har vi även utvecklat en mottagare i .NET som kan ta emot strömmen som både består av video och telemetidata.
+
  * Hur långt har ni kommit?
+ Vi är klara, men det är inte den lösning vi hade önskat oss allra helst.
+ 
  * Vad är nästa steg?
+Nästa steg är att med hjälp av lösningen kunna ta hand om telemetridatat för att bildstabilisera videon.
+ 
  * Några rekommendationer för framtiden?
+Titta på drönartillverkaren Parrots lösningar som verkar göra ungefär det vi vill.
+https://github.com/Parrot-Developers/libvideo-streaming
+Beskrivning: https://developer.parrot.com/docs/pdraw/video-metadata.html#frame-metadata
+ 
  * Några insikter, begränsningar eller utmaningar ni stött på som är intressanta att dela med der av?
+ - Telemetridatan som genereras av Rpanion sker bara en gång per sekund vilket kanske inte är tillräckligt för att kunna använda som underlag vid bildstabilisering.
+ - En hel del tid har gått åt till att undersöka möjligheterna att lägga till telemetridatat i videoströmmens RTP-header - ett spår som vi till slut fick överge.
+ - Vi är osäkra på hur väl videoströmmen och telemetridatan synkar med varandra och har inte haft tid att undersöka det vidare.
 
-<beskrivning.>
 
-
-# Mall för muntlig presentation, deadline dag två kl 12:30
-Den totala tiden av presentation får ni distribuera som ni vill men den måste hållas. Presentation i form av text skall vara i en powerpoint medans demo visar ni som ni vill. Tänk bara på att ni ska hinna på utsatt tid.
-* Överblick och utmaning - 1min
-  - En mening ang vad lösningen gör
-  - Vilken utmaning har ni tacklat?
-* Team - 1min
-  - Vilka är ni i erat team?
-  - Vilka roller har ni haft? Hur har ni jobbat tillsammans?
-* Teknik - 1min
-  - Vilka tekniker har ni använt?
-* Lösning och Demo - 2min 30s
-  - Demo
-  - Hur löser ni denna utmaning?
-  - Vad är nästa steg, rekommendationer för framtiden?
---
+++ instructions/rtsp-server_patched.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 vi:ts=4:noexpandtab
+# Simple RTSP server. Run as-is or with a command-line to replace the default pipeline
+# Taken from https://github.com/tamaggo/gstreamer-examples/blob/master/test_gst_rtsp_server.py
+# gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/video latency=0 ! decodebin ! autovideosink
+
+import sys
+import gi
+import argparse
+import ipaddress
+from netifaces import interfaces, ifaddresses, AF_INET
+
+gi.require_version("Gst", "1.0")
+gi.require_version("GstRtsp", "1.0")
+gi.require_version("GstRtspServer", "1.0")
+from gi.repository import Gst, GstRtspServer, GLib
+
+def ip4_addresses():
+       ip_list = []
+       for interface in interfaces():
+           #print(ifaddresses(interface)[AF_INET])
+           if AF_INET in ifaddresses(interface).keys():
+               for link in ifaddresses(interface)[AF_INET]:
+                   if 'addr' in link.keys():
+                       ip_list.append(link['addr'])
+       return ip_list
+
+def getPipeline(device, height, width, bitrate, format, rotation, framerate):
+    #rotation
+    if device == "rpicam":
+        devrotation = rotation
+    else:
+        devrotation = "videoflip video-direction=identity"
+        if rotation == 90:
+            devrotation = "videoflip video-direction=90r"
+        elif rotation == 180:
+            devrotation = "videoflip video-direction=180"
+        elif rotation == 270:
+            devrotation = "videoflip video-direction=90l"
+    
+    # -1 is no framerate specified
+    if framerate == -1:
+        framestr = ""
+    else:
+        framestr = ",framerate={0}/1".format(framerate)
+        
+    if device == "rpicam":
+            # Old (Buster and earlier) can use the rpicamsrc interface
+            s_src = "rpicamsrc bitrate={0} rotation={3} preview=false ! video/x-h264,width={1},height={2}{4}".format(bitrate*1000, width, height, devrotation, framestr)
+            pipeline_str = "( {s_src} ! queue max-size-buffers=1 name=q_enc ! h264parse ! rtph264pay name=pay0 pt=96 config-interval=1 )".format(**locals())      
+    elif device == "rpicam-uni":
+            # Bullseye uses the new unicam interface ... so need a different pipeline
+            s_src = "libcamerasrc ! capsfilter caps=video/x-raw,width={1},height={2},format=NV12{4},colorimetry=bt601,interlace-mode=progressive ! {3} ! v4l2h264enc extra-controls=\"controls,repeat_sequence_header=1,video_bitrate={0}\" ! video/x-h264,level=(string)4".format(bitrate*1000, width, height, devrotation, framestr)
+            pipeline_str = "( {s_src} ! queue max-size-buffers=1 name=q_enc ! h264parse ! rtph264pay name=pay0 pt=96 config-interval=1 )".format(**locals())
+    elif format == "video/x-raw":
+            s_src = "v4l2src device={0} ! videorate ! {3},width={1},height={2}{5} ! {4} ! videoconvert ! video/x-raw,format=I420".format(device, width, height, format, devrotation, framestr)
+            s_h264 = "x264enc tune=zerolatency bitrate={0} speed-preset=superfast".format(bitrate)
+            pipeline_str = "( {s_src} ! queue max-size-buffers=1 name=q_enc ! {s_h264} ! rtph264pay name=pay0 pt=96 config-interval=1 )".format(**locals())
+    elif format == "video/x-h264":
+            s_src = "v4l2src device={0} ! {3},width={1},height={2}{5} ! {4}".format(device, width, height, format, devrotation, framestr)
+            pipeline_str = "( {s_src} ! queue max-size-buffers=1 name=q_enc ! h264parse ! rtph264pay name=pay0 pt=96 config-interval=1 )".format(**locals())
+    elif format == "image/jpeg":
+            s_src = "v4l2src device={0} ! videorate ! {3},width={1},height={2}{5} ! jpegdec ! {4}".format(device, width, height, format, devrotation, framestr)
+            s_h264 = "x264enc tune=zerolatency bitrate={0} speed-preset=superfast".format(bitrate)
+            pipeline_str = "( {s_src} ! queue max-size-buffers=1 name=q_enc ! {s_h264} ! rtph264pay name=pay0 pt=96 config-interval=1 )".format(**locals())
+    print(pipeline_str)
+    return pipeline_str
+                        
+class MyFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self, device, h, w, bitrate, format, rotation, framerate):
+        GstRtspServer.RTSPMediaFactory.__init__(self)
+        self.device = device
+        self.height = h
+        self.width = w
+        self.bitrate = bitrate
+        self.format = format
+        self.rotation = rotation
+        self.framerate = framerate
+
+    def do_create_element(self, url):
+        pipeline_str = getPipeline(self.device, self.height, self.width, self.bitrate, self.format, self.rotation, self.framerate)
+        return Gst.parse_launch(pipeline_str)
+
+class GstServer():
+    def __init__(self, device,h, w, bitrate, format, rotation, framerate):
+        self.server = GstRtspServer.RTSPServer()
+        f = MyFactory(device, h, w, bitrate, format, rotation, framerate)
+        f.set_shared(True)
+        m = self.server.get_mount_points()
+        m.add_factory("/video", f)
+        self.sourceID = self.server.attach(None)
+
+        print("Server available on rtsp://<IP>:8554/video")
+        print("Use: gst-launch-1.0 rtspsrc location=rtsp://<IP>:8554/video latency=0 ! queue ! decodebin ! autovideosink")
+        print("Where IP is {0}".format(ip4_addresses()))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="RTSP Server using Gstreamer")
+    parser.add_argument("--videosource", help="Video Device", default="/dev/video0", type=str)
+    parser.add_argument("--height", help="Height", default=480, type=int)
+    parser.add_argument("--width", help="Width", default=640, type=int)
+    parser.add_argument("--fps", help="Framerate", default=10, type=int)
+    parser.add_argument("--bitrate", help="bitrate (kbps)", default=2000, type=int)
+    parser.add_argument("--format", help="Video format", default="video/x-raw", type=str)
+    parser.add_argument("--rotation", help="rotation angle", default=0, type=int, choices=[0, 90, 180, 270])
+    parser.add_argument("--udp", help="use UDP sink (dest IP:port) instead of RTSP", default="0:5600", type=str)
+    args = parser.parse_args()
+
+    loop = GLib.MainLoop()
+    Gst.init(None)
+
+    Gst.debug_set_active(True)
+    Gst.debug_set_default_threshold(3)
+
+    if args.udp.split(':')[0] == "0":
+        s = GstServer(args.videosource, args.height, args.width, args.bitrate, args.format, args.rotation, args.fps)
+
+        try:
+            loop.run()
+        except:
+            print("Exiting RTSP Server")
+            loop.quit()
+    else:
+        pipeline_str = getPipeline(args.videosource, args.height, args.width, args.bitrate, args.format, args.rotation, args.fps)
+        pipeline_str += " ! udpsink host={0} port={1}".format(args.udp.split(':')[0], args.udp.split(':')[1])
+        pipeline = Gst.parse_launch(pipeline_str)
+        pipeline.set_state(Gst.State.PLAYING)
+        
+        print("Server sending UDP stream to " + args.udp)
+        print("Use: gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtpjitterbuffer ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink fps-update-interval=1000 sync=false")
+        
+        try:
+            loop.run()
+        except:
+            print("Exiting UDP Server")
+            pipeline.set_state(Gst.State.NULL)
+            loop.quit()
+
